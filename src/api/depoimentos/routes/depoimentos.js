d31daf9ba5cfdd63184af1d552c11105d5378cb0
luangelsdorf <luanferreira2136@gmail.com>
2023-06-24 10:52:56 -0300

    Content updates

--- src/api/depoimentos/routes/depoimentos.js
+++
@@ -1,9 +0,0 @@
-'use strict';
-
-/**
- * depoimentos router
- */
-
-const { createCoreRouter } = require('@strapi/strapi').factories;
-
-module.exports = createCoreRouter('api::depoimentos.depoimentos');
--- src/api/depoimentos/services/depoimentos.js
+++
@@ -1,9 +0,0 @@
-'use strict';
-
-/**
- * depoimentos service
- */
-
-const { createCoreService } = require('@strapi/strapi').factories;
-
-module.exports = createCoreService('api::depoimentos.depoimentos');
